20b3809728803cc676cfb2e590aff0849127f360
s2010458005 <s2010458005@students.fh-hagenberg.at>
2023-07-19 11:55:29 +0200

    embed iframe from autoscout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { HomeComponent } from './components/home/home.component';
+import { ImpressumComponent } from './components/impressum/impressum.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'fahrzeuge', component: CarsComponent },
+  { path: 'über-mich', component: AboutMeComponent },
+  { path: 'kontakt', component: ContactComponent },
+  { path: 'impressum', component: ImpressumComponent },
+  { path: '',   redirectTo: 'home', pathMatch: 'full' }, // redirect to home
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { ImpressumComponent } from './components/impressum/impressum.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    ContactComponent,
+    HomeComponent,
+    CarsComponent,
+    ImpressumComponent
   ],
   imports: [
     BrowserModule,
