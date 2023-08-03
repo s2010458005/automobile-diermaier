4ebc253c79fa438d3d4456986dd9f42fae97a5b4
s2010458005 <s2010458005@students.fh-hagenberg.at>
2023-08-03 12:58:06 +0200

    update header with correct logo and responsiveness

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  showLargeLogo = false;
 
+  constructor() {
+    this.updateViewportWidth(window.innerWidth);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.updateViewportWidth(event.target.innerWidth);
+  }
+
+  updateViewportWidth(width: number) {
+    this.showLargeLogo = width >= 1000;
+  }
 }
